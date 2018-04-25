68628a31efb2811d7fcb1ac588e30155ff900b30
Alberto Hernandez <beto.he7@gmail.com>
2018-04-24 22:41:14 -0500

    remove task and routes task

--- server/api/api.js
+++ server/api/api.js
@@ -3,7 +3,6 @@ var router = require('express').Router();
 // api router will mount other routers
 // for all our resources
 
-router.use('/task', require('./task/taskRoutes'));
 router.use('/user', require('./user/userRoutes'));
 
 module.exports = router;
